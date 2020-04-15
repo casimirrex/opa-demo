4c347e06c3f33d69fcd0403260b096bece3a772b
Param <parameswaranvv@gmail.com>
2020-04-15 10:50:20 -0500

    Fixed test

--- webapp/src/test/java/com.example.opawebdemo/OpaWebDemoApplicationTests.java
+++ webapp/src/test/java/com.example.opawebdemo/OpaWebDemoApplicationTests.java
@@ -2,9 +2,15 @@ package com.example.opawebdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @SpringBootTest
 class OpaWebDemoApplicationTests {
+
+	@MockBean
+	AuthenticationEntryPoint authenticationEntryPoint;
 
 	@Test
 	void contextLoads() {
